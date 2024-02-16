b0bf373588c2e1ffd51f4dbe174640a428c87944
Ethan Shaw <shaweth@flip3.engr.oregonstate.edu>
2024-02-15 18:22:17 -0800

    merged itemP and itemQ

--- DDL.sql
+++ DDL.sql
@@ -1,117 +1,103 @@
 SET FOREIGN_KEY_CHECKS=0;
 SET AUTOCOMMIT = 0;
 
-CREATE OR REPLACE TABLE ItemPrices (
+
+CREATE OR REPLACE TABLE Items (
     itemID INT NOT NULL AUTO_INCREMENT,
     itemName CHAR(50) NOT NULL,
+    quantityOwned INT NOT NULL,
+    size INT NOT NULL,
     listPrice FLOAT NOT NULL,
     PRIMARY KEY (itemID)
 );
 
-INSERT INTO ItemPrices (itemName, listPrice) VALUES
-('skis', 60),
-('snowboard', 60),
-('skiBoots', 25),
-('snowboardBoots', 25),
-('skiPoles', 10);
+INSERT INTO Items (itemName, quantityOwned, size, listPrice) VALUES
+('skis', 30, 120, 60),
+('skis', 30, 125, 60),
+('skis', 40, 130, 60),
+('skis', 50, 135, 60),
+('skis', 60, 140, 60),
+('skis', 70, 145, 60),
+('skis', 80, 150, 60),
+('skis', 90, 155, 60),
+('skis', 90, 160, 60),
+('skis', 90, 165, 60),
+('skis', 90, 170, 60),
+('skis', 90, 175, 60),
+('skis', 70, 180, 60),
+('skis', 60, 185, 60),
+('skis', 60, 190, 60),
+('snowboard', 20, 120, 60),
+('snowboard', 20, 123, 60),
+('snowboard', 25, 126, 60),
+('snowboard', 30, 129, 60),
+('snowboard', 35, 132, 60),
+('snowboard', 40, 135, 60),
+('snowboard', 45, 138, 60),
+('snowboard', 50, 141, 60),
+('snowboard', 55, 144, 60),
+('snowboard', 55, 147, 60),
+('snowboard', 55, 150, 60),
+('snowboard', 55, 153, 60),
+('snowboard', 55, 156, 60),
+('snowboard', 55, 159, 60),
+('snowboard', 55, 162, 60),
+('snowboard', 50, 165, 60),
+('snowboard', 45, 168, 60),
+('snowboard', 35, 171, 60),
+('skiBoots', 5, 22.5, 25),
+('skiBoots', 10, 23, 25),
+('skiBoots', 15, 23.5, 25),
+('skiBoots', 15, 24, 25),
+('skiBoots', 40, 24.5, 25),
+('skiBoots', 55, 25, 25),
+('skiBoots', 70, 25.5, 25),
+('skiBoots', 70, 26, 25),
+('skiBoots', 65, 26.5, 25),
+('skiBoots', 55, 27, 25),
+('skiBoots', 40, 27.5, 25),
+('skiBoots', 40, 28, 25),
+('skiBoots', 55, 28.5, 25),
+('skiBoots', 80, 29, 25),
+('skiBoots', 70, 29.5, 25),
+('skiBoots', 70, 30, 25),
+('skiBoots', 25, 30.5, 25),
+('skiBoots', 35, 31, 25),
+('skiBoots', 25, 31.5, 25),
+('skiBoots', 35, 32, 25),
+('skiBoots', 25, 32.5, 25),
+('snowboardBoots', 5, 22.5, 25),
+('snowboardBoots', 5, 23, 25),
+('snowboardBoots', 10, 23.5, 25),
+('snowboardBoots', 25, 24, 25),
+('snowboardBoots', 35, 24.5, 25),
+('snowboardBoots', 45, 25, 25),
+('snowboardBoots', 50, 25.5, 25),
+('snowboardBoots', 45, 26, 25),
+('snowboardBoots', 35, 26.5, 25),
+('snowboardBoots', 30, 27, 25),
+('snowboardBoots', 25, 27.5, 25),
+('snowboardBoots', 35, 28, 25),
+('snowboardBoots', 55, 28.5, 25),
+('snowboardBoots', 45, 28.5, 25),
+('snowboardBoots', 50, 28.5, 25),
+('snowboardBoots', 20, 28.5, 25),
+('snowboardBoots', 25, 28.5, 25),
+('snowboardBoots', 15, 28.5, 25),
+('snowboardBoots', 5, 28.5, 25),
+('skiPole', 30, 85, 5),
+('skiPole', 40, 90, 5),
+('skiPole', 50, 95, 5),
+('skiPole', 60, 100, 5),
+('skiPole', 60, 105, 5),
+('skiPole', 60, 110, 5),
+('skiPole', 80, 115, 5),
+('skiPole', 80, 120, 5),
+('skiPole', 80, 125, 5),
+('skiPole', 70, 130, 5),
+('skiPole', 50, 135, 5),
+('skiPole', 40, 140, 5);
 
-CREATE OR REPLACE TABLE ItemQuantities (
-    itemQuantityID INT NOT NULL AUTO_INCREMENT,
-    itemID INT NOT NULL,
-    quantityOwned INT NOT NULL,
-    size INT NOT NULL,
-    PRIMARY KEY (itemQuantityID),
-    FOREIGN KEY (itemID) REFERENCES ItemPrices(itemID)
-);
-
-INSERT INTO ItemQuantities (itemID, quantityOwned, size) VALUES
-(1, 30, 120),
-(1, 30, 125),
-(1, 40, 130),
-(1, 50, 135),
-(1, 60, 140),
-(1, 70, 145),
-(1, 80, 150),
-(1, 90, 155),
-(1, 90, 160),
-(1, 90, 165),
-(1, 90, 170),
-(1, 90, 175),
-(1, 70, 180),
-(1, 60, 185),
-(1, 60, 190),
-(2, 20, 120),
-(2, 20, 123),
-(2, 25, 126),
-(2, 30, 129),
-(2, 35, 132),
-(2, 40, 135),
-(2, 45, 138),
-(2, 50, 141),
-(2, 55, 144),
-(2, 55, 147),
-(2, 55, 150),
-(2, 55, 153),
-(2, 55, 156),
-(2, 55, 159),
-(2, 55, 162),
-(2, 50, 165),
-(2, 45, 168),
-(2, 35, 171),
-(3, 5, 22.5),
-(3, 10, 23),
-(3, 15, 23.5),
-(3, 15, 24),
-(3, 40, 24.5),
-(3, 55, 25),
-(3, 70, 25.5),
-(3, 70, 26),
-(3, 65, 26.5),
-(3, 55, 27),
-(3, 40, 27.5),
-(3, 40, 28),
-(3, 55, 28.5),
-(3, 80, 29),
-(3, 70, 29.5),
-(3, 70, 30),
-(3, 25, 30.5),
-(3, 35, 31),
-(3, 25, 31.5),
-(3, 35, 32),
-(3, 25, 32.5),
-(4, 5, 22.5),
-(4, 5, 23),
-(4, 10, 23.5),
-(4, 25, 24),
-(4, 35, 24.5),
-(4, 45, 25),
-(4, 50, 25.5),
-(4, 45, 26),
-(4, 35, 26.5),
-(4, 30, 27),
-(4, 25, 27.5),
-(4, 35, 28),
-(4, 55, 28.5),
-(4, 45, 29),
-(4, 50, 29.5),
-(4, 20, 30),
-(4, 25, 30.5),
-(4, 15, 31),
-(4, 25, 31.5),
-(4, 15, 32),
-(4, 5, 32.5),
-(5, 30, 85),
-(5, 40, 90),
-(5, 50, 95),
-(5, 60, 100),
-(5, 60, 105),
-(5, 60, 110),
-(5, 80, 115),
-(5, 80, 120),
-(5, 80, 125),
-(5, 70, 130),
-(5, 50, 135),
-(5, 40, 140);
 
 CREATE OR REPLACE TABLE Discounts (
     discountID INT NOT NULL AUTO_INCREMENT,
@@ -120,12 +106,12 @@ CREATE OR REPLACE TABLE Discounts (
     PRIMARY KEY (discountID)
 );
 
-
 INSERT INTO Discounts (discountType, discountPercentage) VALUES
 ('student', 10.00),
 ('weekday', 5.00),
 ('allWeek', 20.00),
 ('seasonPass', 50.00);
+
 
 CREATE OR REPLACE TABLE SeasonDates (
   seasonDatesID int NOT NULL AUTO_INCREMENT,
@@ -140,6 +126,7 @@ INSERT INTO SeasonDates (seasonDatesID, seasonStart, seasonEnd) VALUES
 (2023, '2023-10-28', '2024-05-04'),
 (2022, '2022-11-04', '2023-04-27');
 
+
 CREATE OR REPLACE TABLE LiftPassTypes (
   liftPassID int NOT NULL AUTO_INCREMENT,
   category varchar(50) NOT NULL,
@@ -148,14 +135,12 @@ CREATE OR REPLACE TABLE LiftPassTypes (
   PRIMARY KEY (liftPassID)
 );
 
-
 INSERT INTO LiftPassTypes (liftPassID, category, listPrice) VALUES
 (1, 'oneDay', 80.00),
 (2, 'twoDay', 150.00),
 (3, 'threeDay', 220.00),
 (4, 'oneWeek', 500.00),
 (5, 'seasonPass', 700.00);
-
 
 
 CREATE OR REPLACE TABLE Customers (
@@ -172,6 +157,7 @@ INSERT INTO Customers (customerID, firstName, lastName, email, phoneNumber) VALU
 (2, 'Jane', 'Doe', 'jayjay23@gmail.com', '1234567890'),
 (3, 'Jesse', 'Takens', 'jesse@takens.cc', NULL),
 (4, 'Adam', 'Green', 'adamgreen@icl.com', '9255295757');
+
 
 CREATE OR REPLACE TABLE LiftPassTransactions (
     transactionID INT NOT NULL AUTO_INCREMENT,
@@ -193,6 +179,7 @@ INSERT INTO LiftPassTransactions (customerID, seasonDatesID, liftPassID, saleDat
 (4, 3, 2023, '2024-01-25'),
 (3, 3, 2023, '2024-02-01');
 
+
 CREATE OR REPLACE TABLE RentalTransactions (
     transactionID INT NOT NULL AUTO_INCREMENT,
     customerID INT NOT NULL,
@@ -210,6 +197,7 @@ INSERT INTO RentalTransactions (customerID, discountID, saleDate, rentalDuration
 (2, 3, '2024-01-22', 7),
 (3, 2, '2024-01-25', 3),
 (3, NULL, '2024-02-01', 3);
+
 
 CREATE OR REPLACE TABLE RentalItems (
     rentalID INT NOT NULL AUTO_INCREMENT,
@@ -232,6 +220,7 @@ INSERT INTO RentalItems (transactionID, itemQuantityID, quantityRented, sizeRent
 (3, 46, 1, 28.5),
 (4, 10, 2, 165),
 (4, 46, 1, 28.5);
+
 
 
 SET FOREIGN_KEY_CHECKS=1;
