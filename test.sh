3a985ff7fc6e204a08c260565543d021866a7205
Morgan Padberg <morgirme@gmail.com>
2024-03-18 21:53:35 -0700

    Revert "README update"
    This reverts commit 283d1421bf244b8d16a8ee61983ddb2d2671d3e2.

--- test.sh
+++ test.sh
@@ -4,14 +4,14 @@ echo "Moving files"
 rsync -r -v -u --exclude 'node_modules' * osu:~/tmp/
 
 echo "Forwarding Port"
-#ssh -L 3806:127.0.0.1:3806 -N osu &
+ssh -L 3806:127.0.0.1:3806 -N osu &
 
-#echo "Starting server"
-#ssh osu "bash -c 'cd tmp && npm install && npm start'"
+echo "Starting server"
+ssh osu "bash -c 'cd tmp && npm install && npm start'"
 
-#echo ""
-#echo "Killing server"
-#ssh osu "killall -v -u padbergm node"
+echo ""
+echo "Killing server"
+ssh osu "killall -v -u padbergm node"
 
-#echo "Killing forward"
-#kill %%
+echo "Killing forward"
+kill %%
